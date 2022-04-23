85ba666a349559d436c22cb31a610504f291ca4c
ikkundayo <ikuma1006ikuma1006@gmail.com>
2022-04-23 03:49:03 +0000

    update

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -2,9 +2,8 @@ class Public::HomesController < ApplicationController
   def top
     @season = Review.where(season: true)
     @reviews = @season.group(:country_code).where(created_at: Time.current.all_month).order('count(country_code) desc').limit(3)
-     binding.pry
 
-
+    @memories = Memory.where( 'id >= ?', rand(Memory.first.id..Memory.last.id) ).limit(3)
   end
 
   def about
